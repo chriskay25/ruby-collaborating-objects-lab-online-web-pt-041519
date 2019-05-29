5b11bbe44690b1744e88ec8e257a69a993872b8a
Chris Kay <chriskay25@gmail.com>
2019-05-29 14:15:29 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -17,7 +17,9 @@ class MP3Importer
   end 
 
   def import
-    self.files.each{ |filename| Song.new_by_filename(filename) }
+    self.files.each do |filename| 
+      Song.new_by_filename(filename) 
+    end 
   end
   
 end 
