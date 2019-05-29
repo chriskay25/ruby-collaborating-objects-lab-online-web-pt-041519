1ab001c38ec5817d364d031741ec8cb66be7702e
Chris Kay <chriskay25@gmail.com>
2019-05-29 14:58:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,8 @@ class Song
   end 
   
   def artist_name=(name)
-    Artist.find_or_create_by_name(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    self.artist.add_song(self)
   end 
 
 end 
