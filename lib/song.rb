8d0e394126655474d107db259a6d6b467ae33b37
Chris Kay <chriskay25@gmail.com>
2019-05-29 14:53:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,8 @@ class Song
   
   def self.new_by_filename(filename)
     info = filename.chomp(".mp3").split(" - ")
-    song = self.new(info[0]) 
-    song.artist = info[1]
+    song = self.new(info[1]) 
+    song.artist = info[0]
   end 
   
   def artist_name=(name)
